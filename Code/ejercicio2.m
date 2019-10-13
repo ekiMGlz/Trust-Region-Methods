f782b3eb332914d820fc52ba7029d1c47eea554e
Monica Alba <soeur_azmaria@hotmail.com>
2019-10-12 22:06:37 -0500

    ejercicio 2 comentado

--- Code/ejercicio2.m
+++ Code/ejercicio2.m
@@ -1,13 +1,27 @@
+%% Excercise 2. Number of iterations and error.
+
+%We define the Beale function
 beale = @(x)  (1.5 - x(1) + x(1) * x(2))^2    ...
             + (2.25 - x(1) + x(1) * x(2)^2)^2 ...
             + (2.625 - x(1) + x(1) * x(2)^3)^2;
         
+%We define x_0 as the point where ||x_0 - x_opt|| > delta
 x_0 = [-3; -3];
 x_opt = [3; 0.5];
 
+%Calculate the number of itrations, the solution and the result of
+%convergence of the first method starting in x_0.
 [x, msg, i] = mRC1(beale, x_0, 10000)
 
+%Calculate the error between the result of the first method and the optima
+%solution.
 norm(x-x_opt)
 sprintf('RC2')
+
+%Calculate the number of itrations, the solution and the result of
+%convergence of the second method starting in x_0.
 [x, msg, i] = mRC2(beale, x_0, 10000)
+
+%Calculate the error between the result of the second method and the optima
+%solution.
 norm(x-x_opt)
